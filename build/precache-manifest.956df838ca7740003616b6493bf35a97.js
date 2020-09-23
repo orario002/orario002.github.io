231595d8dc79ea36b6b7a2d66b35b92a67ebfc4a
orario002 <orario002@hotmail.com>
2020-09-23 02:39:54 +0100

    Add build to git.

---
+++ build/precache-manifest.956df838ca7740003616b6493bf35a97.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0781911db9c0435a865d350310ff80ad",
+    "url": "/orario002/orario002.github.io/index.html"
+  },
+  {
+    "revision": "a3d65e73f07a44d1f5dd",
+    "url": "/orario002/orario002.github.io/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a0bd54a6c2d3b0138370",
+    "url": "/orario002/orario002.github.io/static/css/main.785c1246.chunk.css"
+  },
+  {
+    "revision": "a3d65e73f07a44d1f5dd",
+    "url": "/orario002/orario002.github.io/static/js/2.c19daad2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/orario002/orario002.github.io/static/js/2.c19daad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0bd54a6c2d3b0138370",
+    "url": "/orario002/orario002.github.io/static/js/main.a2070719.chunk.js"
+  },
+  {
+    "revision": "cb3e5d47c435ef5d47e1",
+    "url": "/orario002/orario002.github.io/static/js/runtime-main.69fcbc52.js"
+  },
+  {
+    "revision": "98f0cacc6bb63b64b98aac7cac082d27",
+    "url": "/orario002/orario002.github.io/static/media/NotoSans-Bold.98f0cacc.ttf"
+  },
+  {
+    "revision": "a1375023bb3f9d55dfc6d58685cd6e69",
+    "url": "/orario002/orario002.github.io/static/media/NotoSans-BoldItalic.a1375023.ttf"
+  },
+  {
+    "revision": "afe1714f485b424f221147e1be6cf8ad",
+    "url": "/orario002/orario002.github.io/static/media/NotoSans-Italic.afe1714f.ttf"
+  },
+  {
+    "revision": "5a87cce84010f7cea085ae218d44a64b",
+    "url": "/orario002/orario002.github.io/static/media/NotoSans-Regular.5a87cce8.ttf"
+  },
+  {
+    "revision": "18bdd4c022c36b3e3eaaba7119b86870",
+    "url": "/orario002/orario002.github.io/static/media/brett-jordan-gOhIsOBCKK0-unsplash.18bdd4c0.jpg"
+  },
+  {
+    "revision": "bde7861d5ad156a5a655c0efa531bf3d",
+    "url": "/orario002/orario002.github.io/static/media/chris-curry-2ckoyzBhv00-unsplash.bde7861d.jpg"
+  },
+  {
+    "revision": "1d93bcb8e51ebafcc20e8a984dac9ec8",
+    "url": "/orario002/orario002.github.io/static/media/logo.1d93bcb8.svg"
+  },
+  {
+    "revision": "9c27587606f805f034e07f5154147353",
+    "url": "/orario002/orario002.github.io/static/media/photo-1528569937393-ee892b976859.9c275876.jpeg"
+  }
+]);
